39b545e63391036d25a60bca153893ee452feca2
Netza Varo <netzahualcoyotl02@gmail.com>
2026-09-06 23:31:53 -0700

    Initialize GameEngine on DOMContentLoaded

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
+import { GameEngine } from './core/GameEngine.js';
 
+window.addEventListener('DOMContentLoaded', () => {
+    // Instanciamos el motor apuntando al div del HTML
+    const engine = new GameEngine('game-container');
+    
+    // Arrancamos todo
+    engine.init();
+});
