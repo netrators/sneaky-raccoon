a1e3c4dee482b3e24f1aee46a7d6d3e7f40686d9
Netza Varo <netzahualcoyotl02@gmail.com>
2026-09-06 23:48:42 -0700

    Add InputManager class for keyboard input handling
    Implement InputManager class to handle keyboard input.

--- src/systems/InputManager.js
+++ src/systems/InputManager.js
@@ -1,1 +1,40 @@
+// src/systems/InputManager.js
 
+export class InputManager {
+    constructor() {
+        this.keys = {
+            forward: false,
+            backward: false,
+            left: false,
+            right: false,
+            sprint: false,
+            interact: false
+        };
+
+        // Escuchadores de eventos de teclado
+        window.addEventListener('keydown', (e) => this.onKeyDown(e));
+        window.addEventListener('keyup', (e) => this.onKeyUp(e));
+    }
+
+    onKeyDown(event) {
+        switch(event.code) {
+            case 'KeyW': case 'ArrowUp': this.keys.forward = true; break;
+            case 'KeyS': case 'ArrowDown': this.keys.backward = true; break;
+            case 'KeyA': case 'ArrowLeft': this.keys.left = true; break;
+            case 'KeyD': case 'ArrowRight': this.keys.right = true; break;
+            case 'ShiftLeft': case 'ShiftRight': this.keys.sprint = true; break;
+            case 'Space': this.keys.interact = true; break;
+        }
+    }
+
+    onKeyUp(event) {
+        switch(event.code) {
+            case 'KeyW': case 'ArrowUp': this.keys.forward = false; break;
+            case 'KeyS': case 'ArrowDown': this.keys.backward = false; break;
+            case 'KeyA': case 'ArrowLeft': this.keys.left = false; break;
+            case 'KeyD': case 'ArrowRight': this.keys.right = false; break;
+            case 'ShiftLeft': case 'ShiftRight': this.keys.sprint = false; break;
+            case 'Space': this.keys.interact = false; break;
+        }
+    }
+}
